8da0ceafa3792ac5e3eea89b1e18a4010cfbef03
Sebastian Mobo <stmobo@gmail.com>
2019-03-27 01:29:48 -0400

    Bot: formatting fixes

--- waifustream/bot_commands.py
+++ waifustream/bot_commands.py
@@ -74,9 +74,9 @@ async def cmd_indexer_status(client, msg, args):
             q_len = await index.get_character_queue_length(client.redis, character)
             n_indexed = await client.redis.scard('character:'+character)
             
-            out = "`{}`: {} items queued".format(character, q_len)
+            out = "`{}`: **{}** items queued".format(character, q_len)
             if n_indexed > 0:
-                out += "{} items indexed".format(n_indexed)
+                out += ", **{}** items indexed".format(n_indexed)
             
             out_lines.append((out, q_len))
         
@@ -90,7 +90,7 @@ async def cmd_indexer_status(client, msg, args):
         q_len = await index.get_character_queue_length(client.redis, character)
         n_indexed = await client.redis.scard('character:'+character)
         
-        out = "`{}`: {} items queued, {} items indexed".format(character, q_len, n_indexed)
+        out = "`{}`: **{}** items queued, **{}** items indexed".format(character, q_len, n_indexed)
         
         return await client.reply(msg, out)
 
